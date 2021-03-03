1847bd95538132fa9b3dfef34d553886c369f719
cfrpg <726525185@qq.com>
2021-03-03 18:29:11 +0800

    update .gitignore

--- src/testerV2/USER/Objects/queue.d
+++
@@ -1,5 +0,0 @@
-.\objects\queue.o: ..\LIB\Queue.c
-.\objects\queue.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\queue.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\queue.o: ..\LIB\Queue.h
-.\objects\queue.o: D:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
